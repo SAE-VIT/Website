417361a7bd808b30bdea90ac115aa4a2fab0e31f
Jitaan <jitaanbanerjee@gmail.com>
2026-02-25 01:41:02 +0530

    added meet the team

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ function Home(){
         <>
             <div className='hero'>
                 <p className="small-left">Engineering the Future</p>
-                <h1>SAE</h1>
+                <h1>SAE-VIT</h1>
                 <p className="small-right">Society of Automotive Engineers</p>
             </div>
 
@@ -20,6 +20,17 @@ function Home(){
                 <Link target="_blank" to="https://www.instagram.com/sae_vit">
                         <img src="./src/assets/x.png" />
                 </Link>
+            </div>
+
+            <div className="team">
+                <div className="team-det">
+                    <h2>Meet the Squad</h2>
+                    <p>Flarmity shongo blipster wazzle cronked through the zindleberry fog while truffins giggled sideways.Plimble snorfed a quasar muffin and jangled the sprocket of lumpledrift curiosity.Wibberwob clocks melted into flanterdash puddles beneath a yondering sky of prizzle. Cranklethorpe fiddled the zazzaroo switch and unleashed seventeen wobbleflutes of sparklegrime. Snizzlepop harmony fluttered onward, bembling the universe with a whooshle of perplexity.
+</p>
+                </div>
+                <div>
+                    <img src = "./src/assets/team.jpeg" className="team-img"/>
+                </div>
             </div>
         </>
     );
